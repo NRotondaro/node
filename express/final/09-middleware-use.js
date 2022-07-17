192e5c14325f2b0de3439336be6269b566314827
NRotondaro <niksonrotondaro1@gmail.com>
2022-07-17 23:06:31 +0000

    middleware module

--- express/final/09-middleware-use.js
+++ express/final/09-middleware-use.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const logger = require('./logger');
 const authorize = require('./authorize');
 
-// 1. use vs route
-// 2. options - our own / express / third party
-
-// app.use([logger, authorize]);
-// app.use(express.static('./public'))
-app.use(morgan('tiny'));
+app.use([logger, authorize]);
 
 app.get('/', (req, res) => {
   res.send('Home');
@@ -24,7 +18,6 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/items', (req, res) => {
-  console.log(req.user);
   res.send('Items');
 });
 
