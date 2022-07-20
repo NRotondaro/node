448aed4692fa2191da5a385d4e6d5e7a1941a9fe
NRotondaro <niksonrotondaro1@gmail.com>
2022-07-20 23:16:48 +0000

    update and delete task implemented

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-require('dotenv').config()
+require('dotenv').config();
 // middleware
-
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
