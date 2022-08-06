a93c32d7918a5821773ce35e71eee59e5b8340df
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-06 11:40:10 +0000

    auth middleware and error with status codes

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -1,10 +1,10 @@
 // check username, password in post(login) request
 // if exist create new JWT
 // send back to front-end
-
 // setup authentication so only the request with JWT can access the dashboard
 
-const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
+const { BadRequestError } = require('../errors');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -14,17 +14,26 @@ const login = async (req, res) => {
   // check in the controller
 
   if (!username || !password) {
-    throw new CustomAPIError('Please provide email and password', 400);
+    throw new BadRequestError('Please provide email and password');
   }
 
-  console.log(username, password);
-  res.send('Fake Login/Register/Signup Route');
+  // just for demo, normally provided by DB!!!
+  const id = new Date().getTime();
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
+
   res.status(200).json({
-    msg: `Hello, Nikson Rotondaro`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
