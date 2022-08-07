d4c7ec0d8b0d68cc35412f80328db7aacda7d95d
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-07 01:03:04 +0000

    implemented the register function in the controller

--- 06-jobs-api/models/User.js
+++ 06-jobs-api/models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const UserSchema = new mongoose.schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide name'],
@@ -20,8 +22,18 @@ const UserSchema = new mongoose.schema({
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
-    maxlength: 12,
   },
 });
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
